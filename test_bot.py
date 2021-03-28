f2b59d3fb68b55d07a2291ebcc33024e1bff4a42
owenspicker <71356125+owenspicker@users.noreply.github.com>
2021-03-27 19:13:23 -0700

    file now tests using PyDictionary

--- test_bot.py
+++ test_bot.py
@@ -1,34 +1,46 @@
 import unittest
-import ChatBot
-
+import ChatBotWithPyDictionary
 
 class TestChatBot(unittest.TestCase):
-
-    # this first function tests the checkPolarity
+  
+    
+    #this first function tests the checkPolarity
     def test_checkPolarity(self):
-        self.assertEqual(ChatBot.checkPolarity("I hate you"), True)
-        self.assertEqual(ChatBot.checkPolarity("Soccer is terrible"), True)
-        self.assertEqual(ChatBot.checkPolarity("I love you"), False)
-        self.assertEqual(ChatBot.checkPolarity("Curling is boring"), True)
-        self.assertEqual(ChatBot.checkPolarity("You couldn't beat me"), False)
-        self.assertEqual(ChatBot.checkPolarity("Crosby is bad at hockey"), True)
-        self.assertEqual(ChatBot.checkPolarity("Messi is a great guy"), False)
-        self.assertEqual(ChatBot.checkPolarity("Hi how are you?"), False)
-        self.assertEqual(ChatBot.checkPolarity("No you could not"), False)
-
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("I hate you"), True)
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("Soccer is terrible"), True)
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("I love you"), False)
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("Curling is boring"), True)
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("You couldn't beat me"), False)
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("Crosby is bad at hockey"), True)
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("Messi is a great guy"), False)
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("Hi how are you?"), False)
+          self.assertEqual(ChatBotWithPyDictionary.checkPolarity("No you could not"), False) 
+          
+          
+          
     def test_checkForCurrency(self):
-        self.assertEqual(ChatBot.checkForCurrency("Can I go to a game for $100?"), True)
-        self.assertEqual(ChatBot.checkForCurrency("£100"), False)
-        self.assertEqual(ChatBot.checkForCurrency("one hundred dollars"), False)
-        self.assertEqual(ChatBot.checkForCurrency("is messi worth $100 mil?"), True)
+          self.assertEqual(ChatBotWithPyDictionary.checkForCurrency("Can I go to a game for $100?"), True)
+          self.assertEqual(ChatBotWithPyDictionary.checkForCurrency("£100"), False) 
+          self.assertEqual(ChatBotWithPyDictionary.checkForCurrency("one hundred dollars"), False)
+          self.assertEqual(ChatBotWithPyDictionary.checkForCurrency("is messi worth $100 mil?"), True)
+
 
     def test_checkForNum(self):
-        self.assertEqual(ChatBot.checkForNum("Who won in 1900?"), True)
-        self.assertEqual(ChatBot.checkForNum("Who won in 2000?"), True)
-        self.assertEqual(ChatBot.checkForNum("Who won in 3000?"), False)
-        self.assertEqual(ChatBot.checkForNum("Who won in 19000?"), False)
-        self.assertEqual(ChatBot.checkForNum("Who won in 190?"), False)
+          self.assertEqual(ChatBotWithPyDictionary.checkForNum("Who won in 1900?"), True)
+          self.assertEqual(ChatBotWithPyDictionary.checkForNum("Who won in 2000?"), True)
+          self.assertEqual(ChatBotWithPyDictionary.checkForNum("Who won in 3000?"), False)
+          self.assertEqual(ChatBotWithPyDictionary.checkForNum("Who won in 19000?"), False)
+          self.assertEqual(ChatBotWithPyDictionary.checkForNum("Who won in 190?"), False)
 
 
+
+
+
+
+    
 if __name__ == '__main__':
-    unittest.main()
+     unittest.main()
+  
+    
+  
+      
